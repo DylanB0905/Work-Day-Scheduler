c39824a9cfd6a1d11c679fe47e4e8deb5b1ca200
dburns98 <dylanburns698@gmail.com>
2022-08-08 17:45:59 -0400

    finished js so now whatever user types in text box will stay after refresh and wrote readme file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = todaysDAte;
 var time = moment().format("HH");
 
+//function to change the color of each row depending on the time of day
 $(".time-div").each(function () {
 var timeDivEl = $(this).attr("id").split("-")[1]
 
@@ -18,3 +19,23 @@ if (time == timeDivEl) {
 }
 
 });
+
+// saves the user input to the local sotrage
+$(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".toDO").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+  });
+
+
+$("#hour-9 .Todo").val(localStorage.getItem("9"));
+$("#hour-10 .Todo").val(localStorage.getItem("10"));
+$("#hour-11 .Todo").val(localStorage.getItem("11"));
+$("#hour-12 .Todo").val(localStorage.getItem("12"));
+$("#hour-13 .Todo").val(localStorage.getItem("13"));
+$("#hour-14 .Todo").val(localStorage.getItem("14"));
+$("#hour-15 .Todo").val(localStorage.getItem("15"));
+$("#hour-16 .Todo").val(localStorage.getItem("16"));
+$("#hour-17 .Todo").val(localStorage.getItem("17"));
+$("#hour-18 .Todo").val(localStorage.getItem("18"));
